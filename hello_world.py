fddf83b39536d0497711f8ed6f6191fd76a95028
wugkqd <wugkqd@gmail.com>
2024-09-12 17:35:01 +0900

    Change hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,8 @@
 def print_hello():
     print("Hello, World!")
 
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+
 if __name__== '__main__':
-    print_hello()
+    print_KIT()
