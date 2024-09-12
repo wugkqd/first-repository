af702aa9f50d389803ed640db0a3fe21839c1a3d
wugkqd <wugkqd@gmail.com>
2024-09-12 17:39:29 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,5 +4,10 @@ def print_hello():
 def print_KIT():
     print("Kumoh National Institute of Technology.")
 
+def add(x, y):
+    a = x+y
+    return a
+
 if __name__== '__main__':
+    print_hello(1,2)
     print_KIT()
